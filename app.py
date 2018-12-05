e25ffc53e94cf8e946926c154937deba17b062b1
Chinmaya Krishnan Mahesh <cmahesh2@illinois.edu>
2018-12-04 20:54:13 -0600

    Add expression cleaning, simplify, factorize, solve

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ from sympy import *
 from sympy.parsing.sympy_parser import parse_expr
 from sympy.parsing.sympy_parser import standard_transformations, implicit_multiplication_application
 from sympy.plotting import plot3d
+from sympy.solvers import solve
 from io import BytesIO
+from re import finditer
+
 import base64
+import re
 
 app = Flask(__name__)
 
@@ -19,8 +23,10 @@ class Action(Enum):
     AREA = 4
     PLOT = 5
     THREED = 6
+    SOLVE = 7
+    SIMPLIFY = 8
 
-possible_actions = {'integrate': Action.INTEGRATE, 'differentiate': Action.DIFFERENTIATE, 'factorize': Action.FACTORIZE, 'area': Action.AREA, 'plot': Action.PLOT, '3d': Action.THREED, '3D': Action.THREED, 'derivate': Action.DIFFERENTIATE, 'integral': Action.INTEGRATE}
+possible_actions = {'integrate': Action.INTEGRATE, 'differentiate': Action.DIFFERENTIATE, 'factorize': Action.FACTORIZE, 'factor': Action.FACTORIZE, 'factors': Action.FACTORIZE, 'area': Action.AREA, 'plot': Action.PLOT, '3d': Action.THREED, '3D': Action.THREED, 'derivate': Action.DIFFERENTIATE, 'integral': Action.INTEGRATE, 'solve': Action.SOLVE, 'root': Action.SOLVE, 'roots': Action.SOLVE, 'simplify': Action.SIMPLIFY}
 
 word_set = set(words.words())
 to_remove = ['x','y','z']
@@ -35,12 +41,24 @@ for i in to_add:
 @app.route('/api/v1.0/<path:command>', methods=['GET'])
 def index(command):
     print("recieved " + command)
+    pattern = "(\d|([A-Z]|[a-z]))[a-z]"
     tokenized_equation = command.split(" ")
     expression = ""
     for i in tokenized_equation:
         if i not in word_set:
             expression += i
     print("expression is " + expression)
+    mult_loc = []
+    for match in finditer(pattern, expression):
+        mult_loc.append(match.span()[1] - 1)
+
+    offset = 0
+    for i in mult_loc:
+        i += offset # to account for the fact that one character is added to the array
+        offset += 1
+        expression = expression[:i] + "*" + expression[i:]
+    expression = expression.replace("^", "**")
+    print("cleaned expression is " + expression)
 
     s = parse_expr(expression)
 
@@ -70,6 +88,12 @@ def index(command):
         graph.save(ramfile)
         ramfile.seek(0)
         image = base64.b64encode(ramfile.getvalue()).decode('ascii')
+    if Action.SOLVE in todo:
+        response = latex(solve(expression))
+    if Action.SIMPLIFY in todo:
+        response = latex(simplify(expression))
+    if Action.FACTORIZE in todo:
+        response = latex(factor(expression))
 
 
     return jsonify({'expression': expression, 'solution': response, 'plot': image})
